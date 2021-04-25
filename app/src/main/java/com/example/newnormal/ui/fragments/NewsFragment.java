6c53c5f4ec6688bedc118949080bc9bb2b5198c5
Kristijan Bošković <kristijan.boskovic96@gmail.com>
2021-04-25 11:11:03 +0200

    Fix indexing bug, place code into methods

--- app/src/main/java/com/example/newnormal/ui/fragments/NewsFragment.java
+++ app/src/main/java/com/example/newnormal/ui/fragments/NewsFragment.java
@@ -1,6 +1,5 @@
 package com.example.newnormal.ui.fragments;
 
-import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
@@ -20,16 +19,15 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.newnormal.R;
 import com.example.newnormal.data.models.News;
-//import com.example.newnormal.data.models.SentimentInfo;
 import com.example.newnormal.ui.activities.MainActivity;
 import com.example.newnormal.ui.activities.NewsArticleActivity;
 import com.example.newnormal.ui.adapters.NewsAdapter;
 import com.google.cloud.language.v1.Sentiment;
 
-import java.text.BreakIterator;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.ListIterator;
 
 public class NewsFragment extends Fragment {
     private static final int NEWS_ARTICLE_REQUEST = 1;
@@ -46,6 +44,7 @@ public class NewsFragment extends Fragment {
 
         // TODO: replace this with more loosely coupled solution (search on Google: "send data from activity to fragment")
         final MainActivity activity = (MainActivity) getActivity();
+        assert activity != null;
         LiveData<List<News>> newsList = activity.getNewsList();
 
         newsList.observe(getViewLifecycleOwner(), new Observer<List<News>>() {
@@ -53,65 +52,9 @@ public class NewsFragment extends Fragment {
             @Override
             public void onChanged(@Nullable List<News> newsList) {
                 // TODO: add ProgressDialog (async)
-                List<String> newsTitlesList = new ArrayList<>();
-                for (News news : newsList) {
-                    newsTitlesList.add(news.getTitle());
-                }
-
-                StringBuilder newsTitlesSb = new StringBuilder();
-                for (int i = 0; i < newsTitlesList.size(); i++) {
-                    String newsTitle = newsTitlesList.get(i);
-
-                    if (Character.isDigit(newsTitle.charAt(0))) { // Remove articles starting with number as they case invalid sentiment sentence reads
-                        newsList.remove(i);
-                    }
-                    else { // Adjust these signs in news articles, as they case invalid sentiment sentence reads
-                        if (newsTitle.contains(".")) {
-                            newsTitle = newsTitle.replace(".", "");
-                        }
-                        if (newsTitle.contains("!")) {
-                            newsTitle = newsTitle.replace("!", "");
-                        }
-                        if (newsTitle.contains("?")) {
-                            newsTitle = newsTitle.replace("?", "");
-                        }
-                        if (newsTitle.contains("$")) {
-                            newsTitle = newsTitle.replace("$", "dollars");
-                        }
-                        if (newsTitle.contains("€")) {
-                            newsTitle = newsTitle.replace("€", "euros");
-                        }
-                        if (newsTitle.contains("£")) {
-                            newsTitle = newsTitle.replace("£", "pounds");
-                        }
-
-                        newsTitlesList.set(i, newsTitle);
-                        newsTitlesSb.append(newsTitle).append(". ");
-                    }
-                }
-                String newsTitlesString = newsTitlesSb.toString().trim();
-
-                // TODO: move this sentiment analysis process somewhere where will be done only once (e.g. MainActivity or NewsViewModel)
-                List<Sentiment> sentiments = activity.performSentimentAnalysisClient(newsTitlesString);
-                if (sentiments.size() == newsList.size()) { // Check if number of sentiments match number of news articles (sentiment analysis done correctly)
-                    Iterator<News> newsIterator = newsList.iterator();
-                    int index = 0;
-                    while (newsIterator.hasNext()) {
-                        newsIterator.next();
-                        Sentiment sentiment = sentiments.get(index);
-                        float sentimentScore = 0;
-                        if (sentiment != null) {
-                            sentimentScore = sentiment.getScore();
-                        }
-                        if (sentimentScore <= 0) {
-                            newsIterator.remove();
-                        }
-                        index++;
-                    }
-                }
-                else {
-                    Toast.makeText(getActivity(),"Sentiment analysis failed, displaying all news articles!",Toast.LENGTH_LONG).show();
-                }
+                assert newsList != null;
+                String newsTitlesString = mergeAllNewsTitles(newsList);
+                filterPositiveNewsTitles(activity, newsList, newsTitlesString);
                 newsAdapter.setNews(newsList);
             }
         });
@@ -126,5 +69,72 @@ public class NewsFragment extends Fragment {
         });
 
         return view;
+    }
+
+    private String mergeAllNewsTitles(List<News> newsList) {
+        List<String> newsTitlesList = new ArrayList<>();
+        for (News news : newsList) {
+            newsTitlesList.add(news.getTitle());
+        }
+
+        StringBuilder newsTitlesSb = new StringBuilder();
+        ListIterator<String> newsTitlesIterator = newsTitlesList.listIterator();
+        while (newsTitlesIterator.hasNext()) {
+            int index = newsTitlesIterator.nextIndex();
+            String newsTitle = newsTitlesIterator.next();
+
+            if (Character.isDigit(newsTitle.charAt(0))) { // Remove articles starting with number as they case invalid sentiment sentence reads
+                newsList.remove(index);
+                newsTitlesIterator.remove();
+            }
+            else { // Adjust these signs in news articles, as they case invalid sentiment sentence reads
+                if (newsTitle.contains(".")) {
+                    newsTitle = newsTitle.replace(".", "");
+                }
+                if (newsTitle.contains("!")) {
+                    newsTitle = newsTitle.replace("!", "");
+                }
+                if (newsTitle.contains("?")) {
+                    newsTitle = newsTitle.replace("?", "");
+                }
+                if (newsTitle.contains("$")) {
+                    newsTitle = newsTitle.replace("$", " dollars");
+                }
+                if (newsTitle.contains("€")) {
+                    newsTitle = newsTitle.replace("€", " euros");
+                }
+                if (newsTitle.contains("£")) {
+                    newsTitle = newsTitle.replace("£", " pounds");
+                }
+
+                newsTitlesList.set(index, newsTitle);
+                newsTitlesSb.append(newsTitle).append(". ");
+            }
+        }
+        return newsTitlesSb.toString().trim();
+    }
+
+    private void filterPositiveNewsTitles(MainActivity activity, List<News> newsList, String newsTitlesString) {
+        // TODO: move this sentiment analysis process somewhere where will be done only once (e.g. MainActivity or NewsViewModel)
+        List<Sentiment> sentiments = activity.performSentimentAnalysisClient(newsTitlesString);
+        if (sentiments.size() == newsList.size()) { // Check if number of sentiments match number of news articles (sentiment analysis done correctly)
+            Iterator<News> newsIterator = newsList.iterator();
+            int index = 0;
+            while (newsIterator.hasNext()) {
+                newsIterator.next();
+                Sentiment sentiment = sentiments.get(index);
+                float sentimentScore = 0;
+                if (sentiment != null) {
+                    sentimentScore = sentiment.getScore();
+                }
+                if (sentimentScore <= 0) {
+                    newsIterator.remove();
+                }
+                index++;
+            }
+        }
+        else {
+//            Toast.makeText(getActivity(),"Sentiment analysis failed, displaying all news articles!",Toast.LENGTH_LONG).show();
+        }
     }
 }
